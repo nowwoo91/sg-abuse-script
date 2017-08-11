c9436ef5524969912a66d8b5ea0f30aeb6e9b9a6
LeeJewoo <nowwoo91@gmail.com>
2017-08-11 16:55:02 +0900

    1.0.0

--- abuselpdb.py
+++ abuselpdb.py
@@ -1,4 +1,6 @@
-import requests, json, argparse, re
+import requests, json, argparse, re, csv
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def check_ip(api_key, ip, days):
     req = "https://www.abuseipdb.com/check/{0}/json?key={1}&days={2}".format(ip, api_key, days)
@@ -7,35 +9,77 @@ def check_ip(api_key, ip, days):
     for i in res:
         print("IP: {0} Country: {1} isoCode: {2} Category: {3} Created: {4}".format(i['ip'],i['country'],i['isoCode'],','.join(str(c) for c in i['category']),i['created']))
 
-def report_ip(api_key, ip, cg, cm=None):
+def report_ip(api_key, ip, cg=None, cm=None):
+    if not cm or cm=='':
+        cm = 'sg_cert_report'
+    if not cg or cg=='':
+        cg = '3,4,9,10,11,14,18,19,20,21,22,23'
     req = "https://www.abuseipdb.com/report/json?key={0}&category={1}&comment={2}&ip={3}".format(api_key,cg,cm,ip)
-    if cm:
-        print('request report ip: {0} category: {1} comment: {2}'.format(ip,','.join(cg),cm))
-    else:
-        print('request report ip: {0} category: {1}'.format(ip,','.join(cg)))
-    res = requests.get(req,verify=True).json()
+    print('request report ip: {0} category: {1} comment: {2}'.format(ip,cg,cm))
+    res = requests.get(req,verify=False).json()
+    try:
+        if res['success']:
+            print('success report ip: {0}'.format(res['ip']))
+    except TypeError as e:
+        print('fail[{0}] report ip: {1} {2}'.format(res[0]['status'],ip,res[0]['meta']))
+
+
+def read_csv(path):   
+    with open(path,'rb') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            yield row
+
+def parse_category(category):
+    if category == '':
+        return
+    ct_result = []
+    ct_list = ['3','4','9','10','11','14','18','19','20','21','22','23']
+    ct_dict = {
+        'fraud orders' :'3',
+        'ddos attack': '4',
+        'open proxy' : '9',
+        'web spam' : '10',
+        'email spam' : '11',
+        'port scan' : '14',
+        'brute-force' : '18',
+        'bad web bot':'19',
+        'exploited host' : '20',
+        'web app attack' : '21',
+        'ssh' : '22',
+        'iot targeted' : '23'
+    }
     
-    if res['success']:
-        print('success report ip: {0}'.format(res['ip']))
-    else:
-        print('fail report')
+    
+    ct_list = category.split(',')
+    for ct in ct_list:
+        ct=ct.strip()
+        ct=ct.lower()
+        if ct.isdigit():
+            if ct in ct_list:
+                ct_result.append(ct)
+            else:
+                print('number error category number is {0} '.format(ct_list))
+        elif ct in ct_dict:
+            
+            ct_result.append(ct_dict[ct])      
+        else:
+            print('category error category name is\n{0}'.format(ct_dict.keys()))
+
+    return ','.join(ct_result)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--day', help="Days", default='30' ,required=False)
-    #parser.add_argument('-k','--key', help="APIKeys", required=True)
-    parser.add_argument('-i','--ip', help="Ip",default='8.8.8.8' ,required=False)
-    parser.add_argument('-cg','--category', help="Report Category 1,2,3... etc",required=False)
-    parser.add_argument('-cm','--comment', help="comment",required=False)
-    parser.add_argument('--check', help="check",action="store_true")
-    parser.add_argument('--report', help="report",action="store_true")
+    # for check
+    parser.add_argument('-k','--key', help="APIKeys", default=None ,required=False)
+    parser.add_argument('-f','--file', help="ip list file path",default='report_ip.csv')
+    parser.add_argument('--report', help="report",default=True,action="store_true")
 
     args = parser.parse_args()
-    key = "oedvorwGLJofgOeoVZda8aS5rJTqJoigo6ufUzxp"
+    if not args.key:
+        key = "oedvorwGLJofgOeoVZda8aS5rJTqJoigo6ufUzxp"
 
-    if args.check:
-        check_ip(api_key=key,ip=args.ip,days=args.day)
-    elif args.report:
-        report_ip(api_key=key,ip=args.ip,cg=args.category,cm="default")
-    else:
-        print('use --check or --report')
+    if args.report:
+        for row in read_csv(args.file):
+            row['category']=parse_category(row['category'])
+            report_ip(api_key=key,ip=row['ip'],cg=row['category'],cm=row['comment'])
